c119434ef2614b18d723cbda5eb94e7db8ce0915
huiweiyong <307468262@qq.com>
2020-05-20 19:05:46 +0800

    简单程序练习

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+import sys
+import os
+# age = int( input("请输入你的年龄：") )
 
-print("欢迎来到python世界")
+def function(a,b):
+    c =a+b
+    print('the c is',c)
+    return
+    # print()
+    # print("This is a function")
+    # a = 1+2
+    # print(a)
+# if age < 18 :
+#     print("警告：你还未成年，不能使用该软件！")
+#     print("未成年人应该好好学习，读个好大学，报效祖国。")
+#     # 为什么sys.exit()出现异常？
+#     # sys.exit()
+#     os._exit(0)
+# else:
+#     print("你已经成年，可以使用该软件。")
+#     print("时间宝贵，请不要在该软件上浪费太多时间。")
+# print("软件正在使用中...")
+if __name__ == '__main__':
+    print()
